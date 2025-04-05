b6ca4c7b58105dace2e9dac4a5139dcc7e579a99
Travis S <Travis.strawb@outlook.com>
2025-04-05 21:54:50 +1030

    inital commit

---
+++ binary_calculator.py
@@ -0,0 +1,13 @@
+def run():
+    print_author_details()
+    pass
+
+def program():
+    print("Running program")
+    pass
+
+def print_author_details():
+    print("Author: Travis Strawbridge\nStudent ID: 110340713\nEmail ID: STRTK001\nThis is my own work as defined by the University's Academic Integrity Policy.\n\n")
+
+if __name__ == "__main__":
+    run()
